c84316b2152da572aaf683f4d3f90a3c0236e925
mass <mass@steamknight.com>
2025-09-01 15:50:41 -0700

    Updated to variable width font
    It's the future, y'all

--- src/states/font_test.c
+++ src/states/font_test.c
@@ -17,7 +17,7 @@ void drawText(Bstring bstr, UWORD uwX, UWORD uwY, UWORD uwMaxWidth, UBYTE ubColo
     fontDestroyTextBitMap(pTextBmp);
     bstrDestroy(&bstr);
 }
-
+static UWORD s_uwFH = 11;
 void fontTestCreate(void)
 {
     logBlockBegin("fontTestCreate");
@@ -33,30 +33,30 @@ void fontTestCreate(void)
 
     drawText(B(">>>"), 0, 0, 10, 1, TX_LEFT_JUSTIFY);
     drawText(B("Left justified text"), 10, 0, 200, 1, TX_LEFT_JUSTIFY);
-    drawText(B("Center justified text"), 10, 10, 200, 9, TX_CENTER_JUSTIFY);
-    drawText(B("Right justified text"), 10, 20, 200, 8, TX_RIGHT_JUSTIFY);
+    drawText(B("Center justified text"), 10, s_uwFH, 200, 9, TX_CENTER_JUSTIFY);
+    drawText(B("Right justified text"), 10, s_uwFH * 2, 200, 8, TX_RIGHT_JUSTIFY);
     drawText(B("<<<"), 210, 0, 10, 1, TX_LEFT_JUSTIFY);
-    drawText(B("This is a longer line that should wrap around to the next line"),   0, 40, 100, 17, TX_LEFT_JUSTIFY);
-    drawText(B("|||||"), 100, 40, 10, 24, TX_LEFT_JUSTIFY);
-    drawText(B("This is a longer line that should wrap around to the next line"), 110, 40, 100, 26, TX_CENTER_JUSTIFY);
-    drawText(B("|||||"), 210, 40, 10, 24, TX_LEFT_JUSTIFY);
-    drawText(B("This is a longer line that should wrap around to the next line"), 220, 40, 100, 27, TX_RIGHT_JUSTIFY);
-    drawText(B("Palette"), 0, 105, 10, 24, TX_LEFT_JUSTIFY);
+    drawText(B("This is a longer line that should wrap around to the next line"),   0, s_uwFH * 4, 100, 17, TX_LEFT_JUSTIFY);
+    drawText(B("|||||"), 103, s_uwFH * 4, 4, 24, TX_LEFT_JUSTIFY);
+    drawText(B("This is a longer line that should wrap around to the next line"), 110, s_uwFH * 4, 100, 26, TX_CENTER_JUSTIFY);
+    drawText(B("|||||"), 213, s_uwFH * 4, 4, 24, TX_LEFT_JUSTIFY);
+    drawText(B("This is a longer line that should wrap around to the next line"), 220, s_uwFH * 4, 100, 27, TX_RIGHT_JUSTIFY);
+    drawText(B("Palette"), 0, s_uwFH * 10 + (s_uwFH >> 1), 10, 24, TX_LEFT_JUSTIFY);
 
     for (UBYTE color = 1; color < 32; ++color)
     {
         UWORD x = 20 + (color / 8) * 30;
-        UWORD y = 100 + (color % 8) * 10;
+        UWORD y = s_uwFH * 10 + (color % 8) * s_uwFH;
         drawText(bstrCreateF(MEMF_FAST, "%02d", color), x, y, 20, color, TX_LEFT_JUSTIFY);
     }
 
     ULONG ulStart = timerGetPrec();
-    drawText(B("This is a long string and it's going to wrap around quite a few times in order to test the worst case performance scenario. Let's see how it does. Right below this line is the time it took to render."), 140, 100, 180, 18, TX_LEFT_JUSTIFY);
+    drawText(B("This is a long string and it's going to wrap around quite a few times in order to test the worst case performance scenario. Let's see how it does. Right below this line is the time it took to render."), 140, s_uwFH * 10, 180, 18, TX_LEFT_JUSTIFY);
     ULONG ulEnd = timerGetPrec();
 
     char timerBuffer[256];
     timerFormatPrec(timerBuffer, timerGetDelta(ulStart, ulEnd));
-    drawText(bstrCreateF(MEMF_FAST, "Rendered in %s ", timerBuffer), 140, 185, 240, 1, TX_LEFT_JUSTIFY);
+    drawText(bstrCreateF(MEMF_FAST, "Rendered in %s ", timerBuffer), 140, s_uwFH * 18 + (s_uwFH >> 1), 240, 1, TX_LEFT_JUSTIFY);
 
     Bstring palette0 = B("00");
     s_pTextBitmap = textCreateFromString(palette0, 20, TX_LEFT_JUSTIFY);
@@ -64,7 +64,7 @@ void fontTestCreate(void)
 
     ULONG ulEndFullPage = timerGetPrec();
     timerFormatPrec(timerBuffer, timerGetDelta(ulStartFullPage, ulEndFullPage));
-    drawText(bstrCreateF(MEMF_FAST, "Whole page rendered in %s ", timerBuffer), 0, 230, 320, 1, TX_CENTER_JUSTIFY);
+    drawText(bstrCreateF(MEMF_FAST, "Whole page rendered in %s ", timerBuffer), 0, 255 - s_uwFH, 320, 1, TX_CENTER_JUSTIFY);
 }
 
 static UBYTE ubColor = 0;
@@ -78,7 +78,7 @@ void fontTestProcess(void)
     
     ulLastTime = timerGet();
 
-    fontDrawTextBitMap(screenGetBackBuffer(g_mainScreen), s_pTextBitmap, 20, 100, ubColor++, FONT_COOKIE);
+    fontDrawTextBitMap(screenGetBackBuffer(g_mainScreen), s_pTextBitmap, 20, s_uwFH * 10, ubColor++, FONT_COOKIE);
     if (ubColor > 31)
         ubColor = 0;
 }
